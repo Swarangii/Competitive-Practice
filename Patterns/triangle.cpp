1c0c99e20004c379e0709d2bde7903dae7809e72
Swarangii <swarangiyadav557@gmail.com>
2025-06-25 18:27:53 +0530

    triangles on code studio

---
+++ Patterns/triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void nForest(int n) {
+	for(int i =1;i<n;i++){
+		for(int j = 1 ; j<=i ; j++){
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main (){
+  int n;
+  cin>>n;
+  nForest(n);
+	return 0;
+}
