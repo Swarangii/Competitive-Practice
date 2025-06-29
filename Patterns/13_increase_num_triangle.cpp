fb6e2cbe9be98a9d73777c33fa05cdf640657cdd
Swarangii <swarangiyadav557@gmail.com>
2025-06-29 23:03:49 +0530

    Increasing Number Triangle Problem on Code Studio

---
+++ Patterns/13_increase_num_triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void nNumberTriangle(int n) {
+    int num = 1;
+    for (int i=1;i<=n;i++){
+        for(int j=1; j<=i;j++){
+            cout<<num <<" ";
+            num ++;
+        }
+        cout<<endl;
+    }
+}
+
+int main (){
+  int n;
+  cin>>n;
+  nNumberTriangle(n);
+	return 0;
+}
