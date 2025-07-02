9cec913082b59f69a8348dc01d9dc4ee89cdb43e
Swarangii <swarangiyadav557@gmail.com>
2025-07-02 23:44:52 +0530

    Palindome on Code Studio , GFG , Leetcode

---
+++ Basic Math/Palindrome/Palindrom.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+bool palindrome(int n)
+{
+    int dup = n;
+	int revNum = 0;
+	while(dup>0){
+		int lastdigit = dup%10;
+		revNum = ((revNum*10)+lastdigit);
+		dup=dup/10;
+	}
+    if (revNum == n){
+        return true;
+    }
+    else return false;
+}
+int main(){
+    int n;
+    cin>>n;
+    palindrome(n);
+    return 0;
+}
