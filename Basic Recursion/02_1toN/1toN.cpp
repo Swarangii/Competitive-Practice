7f66796d58a4994d735aba4d052e417266a1ab01
Swarangii <swarangiyadav557@gmail.com>
2025-07-07 18:28:03 +0530

    Print Number from 1 to N without using while loop

---
+++ Basic Recursion/02_1toN/1toN.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void linearly(int i, int n){
+    if (i>n) return;
+    cout<<i<<" ";
+    linearly(i+1,n);
+}
+
+vector<int> printNos(int x) {
+    linearly(1,x);
+    return {};
+}
+
+
+int main() {
+	int n;
+	cin>>n;
+  printNos(n);
+}
