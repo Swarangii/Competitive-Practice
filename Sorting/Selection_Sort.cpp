dd8f5124a1b1a17f7ae530ff8998021610f44d26
Swarangii <swarangiyadav557@gmail.com>
2025-07-21 23:38:53 +0530

    Selction Sort on Code Studio

---
+++ Sorting/Selection_Sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+// Highest / Lowest Frequency Elements on Code Studio
+
+void selectionSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i = 0; i<=n-1 ; i++){
+        int mini = i;
+        for (int j = i+1; j<n ;j++){
+            if(arr[j] < arr[mini]){
+                mini=j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
+    }
+}
+int main() {
+  vector<int> v;
+  selectionSort(v);
+  return 0;
+}
