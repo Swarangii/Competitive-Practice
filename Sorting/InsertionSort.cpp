671677419627aca6d6f50fdf01141f6bcde5caba
Swarangii <swarangiyadav557@gmail.com>
2025-07-22 00:03:07 +0530

    Insertion Sort

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+void insertionSort(int n, int arr[]){
+    // Write your code here.
+    for (int i = 0; i<n ; i++){
+        int key = arr[i];
+        int j = i - 1;
+        while(j>=0 && arr[j] > key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+int main() {
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  for(int i=0;i<n;i++){
+    cout<<arr[i];
+  }
+  insertionSort(n,arr);
+  return 0;
+}
